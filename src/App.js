49bd7af937e9e35fe78c94cf737e27f10aff9b04
Ron Almog <ron8132@gmail.com>
2023-03-05 22:02:12 -0500

    add router and some basic routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NoMatch.js
@@ -0,0 +1,10 @@
+const NoMatch = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h3>This page cannot be found</h3>
+    </div>
+  );
+};
+
+export default NoMatch;
