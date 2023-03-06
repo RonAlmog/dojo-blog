3151465dafb755434ddbc27c17c375c8d18962a6
Ron Almog <ron8132@gmail.com>
2023-03-06 18:45:02 -0500

    add delete call

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
   let { id } = useParams();
@@ -8,6 +9,15 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const navigate = useNavigate();
+  const deletePost = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -17,6 +27,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={deletePost}>Delete</button>
         </article>
       )}
     </div>
