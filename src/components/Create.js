dddaa29111af6bdbfa9156ea57732955b026ec66
Ron Almog <ron8132@gmail.com>
2023-03-06 15:16:23 -0500

    build the form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,42 @@
+import { useState } from "react";
+
 const Create = () => {
-  return <div>Create a new blog post!</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  return (
+    <div className="create">
+      <h2>Create a new blog post!</h2>
+      <form>
+        <label>Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Content:</label>
+        <textarea
+          rows="3"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Author:</label>
+        <select
+          name=""
+          id=""
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
